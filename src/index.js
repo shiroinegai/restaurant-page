e20bc15d14560b32d9150dd61d415992bb1de6a7
Shiroi Negai <shiroinegai@pm.me>
2021-08-28 07:26:42 +0800

    Initialise project files

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./main.css";
+
+console.log("Restaurant page is running.");
+
+const content = document.getElementById("content");
+
+const headerText = document.createElement("h1");
+headerText.append("Restaurant");
+
+content.append(headerText);
